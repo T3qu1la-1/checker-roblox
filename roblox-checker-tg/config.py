a23abf92a9db1270038437fadc383ff878780fae
T3qu1la-1 <manobrown333011@gmail.com>
2025-09-30 13:14:13 -0300

    Update config.py

--- roblox-checker-tg/config.py
+++ roblox-checker-tg/config.py
@@ -1,4 +1,4 @@
-TOKEN = "SEU_TOKEN_AQUI"
-ADMIN_ID = 123456789          # seu Telegram ID
+TOKEN = "7356891537:AAHHYDE9qFSoHIkLFaNAX2UJzMcOa0u_qcE"
+ADMIN_ID = 7898948145          # seu Telegram ID
 WORKERS = 200                 # corotinas simultâneas
 TIMEOUT = 8                   # segundos por request
