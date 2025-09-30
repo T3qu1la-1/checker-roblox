85923cfad86db3a66c893fd0d24584fcb7f06045
T3qu1la-1 <manobrown333011@gmail.com>
2025-09-30 13:11:53 -0300

    Create config.py

---
+++ roblox-checker-tg/config.py
@@ -0,0 +1,4 @@
+TOKEN = "SEU_TOKEN_AQUI"
+ADMIN_ID = 123456789          # seu Telegram ID
+WORKERS = 200                 # corotinas simultâneas
+TIMEOUT = 8                   # segundos por request
